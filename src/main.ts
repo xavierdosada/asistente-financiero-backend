666c7d5dfe34ab5dd47bc125de8e6a122bf7c73d
igtdev <igtdesarrollo@gmail.com>
2026-04-24 17:20:38 -0300

    fixed/render-deploy

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: true });
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  const port = Number(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
